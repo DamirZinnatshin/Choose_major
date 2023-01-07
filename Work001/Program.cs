ecdeb38977819a270f7b6abd1c3084cec8de3052
Damir <dzinnatshin@gmail.com>
2023-01-07 22:57:30 +0300

    Добавил решение в Program.cs

--- Work001/Program.cs
+++ Work001/Program.cs
@@ -1,4 +1,14 @@
-﻿
+﻿string[] array = {"Moscow","All","4","France","U","Great"};
+int x = array.Length;
+string result = "random_string";
+for (int i = 0; i < x; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        result = array[i];
+        Console.Write(result + " ");
+    }
+}
 
 
 
